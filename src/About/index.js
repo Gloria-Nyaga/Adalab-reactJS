41ea8682fba8f5a8be80ebd53edf3f1f12e910be
Gloria-Nyaga <nyagagakiigloria@gmail.com>
2024-06-20 16:06:25 +0300

    added files

---
+++ src/About/index.js
@@ -0,0 +1,13 @@
+import "./index.css";
+
+const About = () =>{
+    return(
+      <div>
+        <h1>About Us</h1>
+        <p>We are learning about ReactJS</p>
+      </div>
+    );
+};
+
+  
+export default About;
